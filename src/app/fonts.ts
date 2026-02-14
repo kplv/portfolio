476145c6c19ddb24f1287d0dd27ee6dbb000bbf5
kplv <denuc.kopylov@gmail.com>
2026-02-14 16:40:13 +0100

    fix: font loading

---
+++ src/app/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local'
+
+export const abcFavorit = localFont({
+  src: '../../public/fonts/ABCFavoritVariable-Trial.woff2',
+  variable: '--font-favorit',
+  display: 'swap',
+})
